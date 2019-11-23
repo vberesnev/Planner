41fe1b393ea2585851c6d92cd0035cfc079a8262
Valeriy Beresnev <vberesnev.job@gmail.com>
2019-11-23 17:18:07 +0300

    Добавлены класс ноды и двусвязного кольцевого списка для "кругового"  вывода информации (месяц, день недели)

---
+++ Planner/Model/DoublyNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner.Model
+{
+    public class DoublyNode<T>
+    {
+        public T Data { get; private set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ Planner/Model/DoublyNodeLinkedList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner.Model
+{
+    public class DoublyNodeLinkedList<T> : IEnumerable<T>
+    {
+        private DoublyNode<T> head;
+        private DoublyNode<T> current;
+        private int count;
+
+        public void Add(DoublyNode<T> node)
+        {
+            if (head == null)
+            {
+                head = node;
+                head.Previous = node;
+                head.Next = node;
+            }
+            else
+            {
+                node.Previous = head.Previous;
+                node.Next = head;
+                head.Previous.Next = node;
+                head.Previous = node;
+            }
+            count++;
+        }
+
+        public DoublyNode<T> Current(T data)
+        {
+            if (head == null) return null;
+            current = head;
+            do
+            {
+                if (current.Data.Equals(data))
+                    return current;
+                current = current.Next;
+            }
+            while (current != head);
+            return null;
+        }
+
+        public DoublyNode<T> MoveNext()
+        {
+            current = current.Next;
+            return current;
+        }
+
+        public DoublyNode<T> MovePrevious()
+        {
+            current = current.Previous;
+            return current;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            count = 0;
+        }
+
+        public int Count => count;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            current = head;
+            do
+            {
+                if (current != null)
+                {
+                    yield return current.Data;
+                    current = current.Next;
+                }
+            }
+            while (current != head);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
